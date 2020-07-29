903e181687242e5b7bf2aa88aed79ebbc5f5a1ee
reiss-josh <reiss.joshuae@gmail.com>
2020-07-29 13:19:52 -0700

    Decided on size of character capsule

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+//on every frame, move the camera to the same position as the player
+//apparently this is good
+public class MoveCamera : MonoBehaviour
+{
+    public Transform player;
+    void Update() {
+        this.transform.position = player.transform.position;
+    }
+}
